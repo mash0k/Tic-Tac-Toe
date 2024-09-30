617010c6695141cc40145bce21fc1faaa35bdffa
mash0k <28.maria.cojocaru@gmail.com>
2024-09-30 21:01:17 +0300

    Revert "Lab02"

--- Player.hpp
+++ Player.hpp
@@ -5,12 +5,21 @@
 class Player
 {
 private:
-    std::string name;  
+    std::string name;
 
 public:
-    Player(const std::string& playerName);
+    Player(const std::string& playerName) : name(playerName) {}
 
-    int readMove() const;
+    int readMove() const
+    {
+        int move;
+        std::cout << name << ", introdu rândul și coloana (1-3): ";
+        std::cin >> move;
+        return move - 1; // Convertim la index 0-based
+    }
 
-    std::string getName() const;
+    std::string getName() const
+    {
+        return name;
+    }
 };
